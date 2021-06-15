c9932ea3d282507d2e6f444cccd64fdb2a0c24b9
RamNewton <32165686+RamNewton@users.noreply.github.com>
2021-06-15 11:21:19 +0530

    Extracted Business Logic from routes into controller for password.js

---
+++ controller/passwords.js
@@ -0,0 +1,103 @@
+const Joi = require("joi");
+const Password = require("../model/user");
+const { encrypt, decrypt } = require("../utils/encryption");
+
+const validate = (data) => {
+    const schema = Joi.object({
+        accountName: Joi.string().required().min(2).max(255),
+        username: Joi.string().allow("").max(255),
+        password: Joi.string().required(),
+    });
+
+    return schema.validate(data);
+};
+
+exports.createResource = async (req, res) => {
+    const { error } = validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    const userPasswordsDb = Password.doc(req.user.email).collection("passwords");
+    const accountsQuery = userPasswordsDb.where("accountName", "==", req.body.accountName);
+
+    accounts = await accountsQuery.get();
+    if (accounts.docs.length > 0) return res.status(400).send("An entry with this account name is already present.");
+
+    let passwordBuffer = Buffer.from(req.body.password, "utf-8");
+    let keyBuffer = Buffer.from(process.env.encryptKey, "utf-8");
+    let encryptedMessageBuffer = encrypt(passwordBuffer, keyBuffer);
+
+    await userPasswordsDb.add({
+        accountName: req.body.accountName,
+        username: req.body.username,
+        password: encryptedMessageBuffer.toString("hex"),
+    });
+
+    passwordBuffer = null;
+    encryptedMessageBuffer = null;
+    keyBuffer = null;
+
+    res.status(200).send();
+};
+
+exports.getStore = async (req, res) => {
+    const userPasswordsDb = Password.doc(req.user.email).collection("passwords");
+    let accounts = await userPasswordsDb.get();
+
+    let keyBuffer = Buffer.from(process.env.encryptKey, "utf-8");
+
+    try {
+        if (accounts.size > 0) {
+            accounts = accounts.docs.map((doc) => {
+                let data = doc.data();
+                let encryptedMessageBuffer = Buffer.from(data.password, "hex");
+                let passwordBuffer = decrypt(encryptedMessageBuffer, keyBuffer);
+                return { ...data, password: passwordBuffer.toString(), id: doc.id };
+            });
+        } else {
+            accounts = [];
+        }
+    } catch (err) {
+        console.log(err);
+    }
+
+    res.status(200).send(accounts);
+};
+
+exports.updateResource = async (req, res) => {
+    const { error } = validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    const id = req.params.id;
+    const userPasswordsDb = Password.doc(req.user.email).collection("passwords");
+    const accountRef = userPasswordsDb.doc(id);
+    let account = await accountRef.get();
+
+    if (!account.exists) res.status(400).send("Entry for this account does not exist.");
+
+    let passwordBuffer = Buffer.from(req.body.password, "utf-8");
+    let keyBuffer = Buffer.from(process.env.encryptKey, "utf-8");
+    let encryptedMessageBuffer = encrypt(passwordBuffer, keyBuffer);
+
+    await accountRef.set({
+        accountName: req.body.accountName,
+        username: req.body.username,
+        password: encryptedMessageBuffer.toString("hex"),
+    });
+
+    passwordBuffer = null;
+    encryptedMessageBuffer = null;
+    keyBuffer = null;
+
+    res.status(200).send();
+};
+
+exports.deleteResource = async (req, res) => {
+    const passwordsRef = Password.doc(req.user.email).collection("passwords");
+    const accountRef = passwordsRef.doc(req.params.id);
+    let account = await accountRef.get();
+
+    if (!account.exists) res.status(400).send("Entry for this account does not exist.");
+
+    await accountRef.delete();
+    res.status(200).send();
+};
--- routes/passwords.js
+++ routes/passwords.js
@@ -1,109 +1,15 @@
 const config = require("config");
 const express = require("express");
 const router = express.Router();
-const Joi = require("joi");
-const Password = require("../model/user");
 const auth = require("../middleware/auth");
-const { encrypt, decrypt } = require("../utils/encryption");
+const { createResource, getStore, updateResource, deleteResource } = require("../controller/passwords");
 
-router.post("/", auth, async (req, res) => {
-    const { error } = validate(req.body);
-    if (error) return res.status(400).send(error.details[0].message);
+router.post("/", auth, createResource);
 
-    const userPasswordsDb = Password.doc(req.user.email).collection("passwords");
-    const accountsQuery = userPasswordsDb.where("accountName", "==", req.body.accountName);
+router.get("/", auth, getStore);
 
-    accounts = await accountsQuery.get();
-    if (accounts.docs.length > 0) return res.status(400).send("An entry with this account name is already present.");
+router.put("/:id", auth, updateResource);
 
-    let passwordBuffer = Buffer.from(req.body.password, "utf-8");
-    let keyBuffer = Buffer.from(process.env.encryptKey, "utf-8");
-    let encryptedMessageBuffer = encrypt(passwordBuffer, keyBuffer);
-
-    await userPasswordsDb.add({
-        accountName: req.body.accountName,
-        username: req.body.username,
-        password: encryptedMessageBuffer.toString("hex"),
-    });
-
-    passwordBuffer = null;
-    encryptedMessageBuffer = null;
-    keyBuffer = null;
-
-    res.status(200).send();
-});
-
-router.get("/", auth, async (req, res) => {
-    const userPasswordsDb = Password.doc(req.user.email).collection("passwords");
-    let accounts = await userPasswordsDb.get();
-
-    let keyBuffer = Buffer.from(process.env.encryptKey, "utf-8");
-
-    try {
-        if (accounts.size > 0) {
-            accounts = accounts.docs.map((doc) => {
-                let data = doc.data();
-                let encryptedMessageBuffer = Buffer.from(data.password, "hex");
-                let passwordBuffer = decrypt(encryptedMessageBuffer, keyBuffer);
-                return { ...data, password: passwordBuffer.toString(), id: doc.id };
-            });
-        } else {
-            accounts = [];
-        }
-    } catch (err) {
-        console.log(err);
-    }
-
-    res.status(200).send(accounts);
-});
-
-router.put("/:id", auth, async (req, res) => {
-    const { error } = validate(req.body);
-    if (error) return res.status(400).send(error.details[0].message);
-
-    const id = req.params.id;
-    const userPasswordsDb = Password.doc(req.user.email).collection("passwords");
-    const accountRef = userPasswordsDb.doc(id);
-    let account = await accountRef.get();
-
-    if (!account.exists) res.status(400).send("Entry for this account does not exist.");
-
-    let passwordBuffer = Buffer.from(req.body.password, "utf-8");
-    let keyBuffer = Buffer.from(process.env.encryptKey, "utf-8");
-    let encryptedMessageBuffer = encrypt(passwordBuffer, keyBuffer);
-
-    await accountRef.set({
-        accountName: req.body.accountName,
-        username: req.body.username,
-        password: encryptedMessageBuffer.toString("hex"),
-    });
-
-    passwordBuffer = null;
-    encryptedMessageBuffer = null;
-    keyBuffer = null;
-
-    res.status(200).send();
-});
-
-router.delete("/:id", auth, async (req, res) => {
-    const passwordsRef = Password.doc(req.user.email).collection("passwords");
-    const accountRef = passwordsRef.doc(req.params.id);
-    let account = await accountRef.get();
-
-    if (!account.exists) res.status(400).send("Entry for this account does not exist.");
-
-    await accountRef.delete();
-    res.status(200).send();
-});
-
-function validate(req) {
-    const schema = Joi.object({
-        accountName: Joi.string().required().min(2).max(255),
-        username: Joi.string().allow("").max(255),
-        password: Joi.string().required(),
-    });
-
-    return schema.validate(req);
-}
+router.delete("/:id", auth, deleteResource);
 
 module.exports = router;
