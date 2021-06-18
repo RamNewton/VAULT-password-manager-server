09928a9b501827bae84e69ac430141423cb48f34
RamNewton <32165686+RamNewton@users.noreply.github.com>
2021-06-18 15:21:14 +0530

    Added production specific code

--- index.js
+++ index.js
@@ -9,10 +9,20 @@ const app = express();
 
 app.use(morgan("dev"));
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 app.use(cookieParser());
+
+if (process.env.NODE_ENV === "production") app.use(express.static(path.join(__dirname, "client/build")));
+if (process.env.NODE_ENV !== "production") app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+
 app.use("/api/auth/", authRouter);
 app.use("/api/main/", dashboardRouter);
+
+if (process.env.NODE_ENV === "production") {
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname + "/client/build/index.html"));
+    });
+}
+
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
